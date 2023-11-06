44ddb0c4fca9b5ecae0c283559cf49fb833d47b4
Rolande <89341905+Lande21@users.noreply.github.com>
2023-11-05 21:00:27 -0600

    Added the time zones

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -3,6 +3,9 @@ import './quotedisplay.css';
 
 function QuoteDisplay() {
   const [quote, setQuote] = useState('');
+  // current time
+  const [centralTime, setCentralTime] = useState('');
+  const [kigaliTime, setKigaliTime] = useState('');
 
   // Function to fetch a random quote
   const fetchRandomQuote = () => {
@@ -15,13 +18,59 @@ function QuoteDisplay() {
   useEffect(() => {
     // Fetch a random quote when the component mounts
     fetchRandomQuote();
+    //fecth 
+    const fetchCentralTime = async () => {
+      try {
+        const response = await fetch('http://worldtimeapi.org/api/timezone/America/Chicago');
+        if (response.ok) {
+          const data = await response.json();
+          const currentDateTime = new Date(data.utc_datetime);
+          setCentralTime(currentDateTime.toLocaleTimeString('en-US'));
+          
+        } else {
+          throw new Error('Failed to fetch Central Time');
+        }
+      } catch (error) {
+        console.error('Error fetching central time:', error);
+      }
+    };
+    fetchCentralTime();
+    const fetchKigaliTime = async () => {
+      try {
+        const response = await fetch('http://worldtimeapi.org/api/timezone/Africa/Kigali');
+        if (response.ok) {
+          const data = await response.json();
+          // Get the time in Kigali, Rwanda
+          const currentDateTime = new Date(data.utc_datetime);
+          // Adjust for the time difference (8 hours ahead of Central US time)
+          currentDateTime.setHours(currentDateTime.getHours() + 8);
+          //const currentDateTime = new Date(data.datetime);
+          setKigaliTime(currentDateTime.toLocaleTimeString('en-US'));
+        } else {
+          throw new Error('Failed to fetch Kigali Time');
+        }
+      } catch (error) {
+        console.error('Error fetching Kigali time:', error);
+      }
+    };
+
+    fetchKigaliTime();
   }, []);
+
 
   return (
     <div>
+      
       <h1>Wisdom of the Day</h1>
-      <p className="quote">{quote}</p>
+      <div class="quote-container">
+      <blockquote className="quote">{quote}
+      </blockquote>
+      </div>
       <button onClick={fetchRandomQuote} className="button">Generate Random Quote</button>
+      <h2 className="mytime">Current Central Time (US)</h2>
+      <p className="mytime">{centralTime}</p>
+      <h2 className="mytime">Current Time in Kigali, Rwanda</h2>
+      <p className="mytime">{kigaliTime}</p>
     </div>
   );
 }
