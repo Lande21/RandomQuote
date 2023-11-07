2760ebe7549214b5fb12e230cf3dcf2902a97a29
Rolande <89341905+Lande21@users.noreply.github.com>
2023-11-06 22:03:31 -0600

    Newest updates

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -61,7 +61,7 @@ function QuoteDisplay() {
   return (
     <div>
       
-      <h1>Wisdom of today, November 6, 2023</h1>
+      <h1>Wisdom of today: Monday November 6, 2023</h1>
       <div class="quote-container">
       <blockquote className="quote">{quote}
       </blockquote>
