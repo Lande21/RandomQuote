0807ce03621258961cf465a2727c890abfb85ae5
Rolande <89341905+Lande21@users.noreply.github.com>
2023-11-08 21:00:01 -0600

    new exercises

--- src/Practicetwo.js
+++ src/Practicetwo.js
@@ -2,27 +2,27 @@ import React, { useState, useEffect } from 'react';
 import './quotedisplay.css'; 
 
 function Practicetwo() {
-    const [users, setUsers] = useState([]);
+    const [pokemons, setPokemons] = useState([]);
     useEffect(() => {
         fetch('https://pokeapi.co/api/v2/pokemon')
-        .then(response => response.json())
-        .then(data => {
-            setUsers(data);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+            .then(response => response.json())
+            .then(data => {
+                setPokemons(data.results);//accessing the 'results' array
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
 
     }, []);
+
     return (
         <div className="exercises">
             <h3>Here is my second practice with REST APIs</h3>
-          <h1>Pokemons</h1>
-          {users.map(pokemon => (
+            <h1>Pokemons</h1>
+            {pokemons.map(pokemon => (
              
-            <h3>{pokemon.name}</h3>
+            <h3 key={pokemon.name}>{pokemon.name}</h3>
            
-          
         ))}
         </div>
     );
