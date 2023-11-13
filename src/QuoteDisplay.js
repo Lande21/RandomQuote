a541ad4ee6d0b2ffa5629f4cba459a54b6278968
Rolande <89341905+Lande21@users.noreply.github.com>
2023-11-12 20:54:44 -0600

    Adding deals

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -61,7 +61,7 @@ function QuoteDisplay() {
   return (
     <div>
       
-      <h1>Wisdom of Today: Staurday November 11, 2023</h1>
+      <h1>Wisdom of Today: SUnday November 12, 2023</h1>
       <div class="quote-container">
       <blockquote className="quote">{quote}
       </blockquote>
