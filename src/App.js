9bd1d6e0e4e06e048f2adccaf88ac7fc6c6498a3
Rolande <89341905+Lande21@users.noreply.github.com>
2023-11-17 23:54:21 -0600

    Adding a count down

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import CountdownTimer from './CountdownTimer';
 import Practicetwo from './Practicetwo';
 import QuoteDisplay from './QuoteDisplay';
 import UnsplashGallery from './Unsplashprac';
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <QuoteDisplay />
-        
+        <CountdownTimer />
         
         <UnsplashGallery />
        
