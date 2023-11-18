30c641c15a6b09e0285033142ae5f250b163bf31
Rolande <89341905+Lande21@users.noreply.github.com>
2023-11-17 23:52:33 -0600

    Adding a count down

---
+++ src/CountdownTimer.js
@@ -0,0 +1,29 @@
+// CountdownTimer.js
+import React, { useState, useEffect } from 'react';
+
+const CountdownTimer = ({ initialSeconds }) => {
+  const [seconds, setSeconds] = useState(initialSeconds);
+
+  useEffect(() => {
+    // Exit the effect if the countdown reaches 0
+    if (seconds <= 0) {
+      return;
+    }
+
+    // Update the countdown every second
+    const timerId = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds - 1);
+    }, 1000);
+
+    // Clean up the interval when the component is unmounted
+    return () => clearInterval(timerId);
+  }, [seconds]);
+
+  return (
+    <div>
+      <h1>Countdown: {seconds} seconds</h1>
+    </div>
+  );
+};
+
+export default CountdownTimer;
