7b676874ab7940be2636472e5c149915ce63bb80
Rolande <89341905+Lande21@users.noreply.github.com>
2023-11-19 20:52:27 -0600

    Adding a 3 min count down

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -61,7 +61,7 @@ function QuoteDisplay() {
   return (
     <div>
       
-      <h1>Wisdom of Today: Saday November 18, 2023</h1>
+      <h1>Wisdom of Today: Suday November 18, 2023</h1>
       <div class="quote-container">
       <blockquote className="quote">{quote}
       </blockquote>
