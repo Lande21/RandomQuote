f9d753b53284b9d2cfec4bdeac65ca0c1d798178
Rolande <89341905+Lande21@users.noreply.github.com>
2023-11-25 17:53:20 -0600

    Adding daily changes

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -61,7 +61,7 @@ function QuoteDisplay() {
   return (
     <div>
       
-      <h1>Wisdom of Today: Saturday on November 25, 2023 ~~HAPPY Black Friday</h1>
+      <h1>Wisdom of Today: Saturday on November 25, 2023 ~~HAPPY BIRTH</h1>
       <div class="quote-container">
       <blockquote className="quote">{quote}
       </blockquote>
