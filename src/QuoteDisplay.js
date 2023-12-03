2d3654792037658d5f623ecf7c44808078859e01
Rolande <89341905+Lande21@users.noreply.github.com>
2023-12-02 20:41:06 -0600

    adding target date

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -61,7 +61,7 @@ function QuoteDisplay() {
   return (
     <div>
       
-      <h1>Wisdom of Today: Satay on December 1, 2023 Hello December :)</h1>
+      <h1>Wisdom of Today: Saturday on December 1, 2023 Hello December :)</h1>
       <div class="quote-container">
       <blockquote className="quote">{quote}
       </blockquote>
