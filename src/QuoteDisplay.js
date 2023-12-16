65a0840755a2636f762ad04a9ca40737e9206d5d
Rolande <89341905+Lande21@users.noreply.github.com>
2023-12-15 21:16:11 -0600

    changes for thursday

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -61,7 +61,7 @@ function QuoteDisplay() {
   return (
     <div>
       
-      <h1>Wisdom of Today: Thursday on December 14, 2023 </h1>
+      <h1>Wisdom of Today: fday on December 14, 2023 </h1>
       <div class="quote-container">
       <blockquote className="quote">{quote}
       </blockquote>
