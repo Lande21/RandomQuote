db51910b5db567d0ae0609bb53ee5bf4bd57c1cc
Rolande <89341905+Lande21@users.noreply.github.com>
2024-01-03 21:49:35 -0600

    changes of the date

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -61,7 +61,7 @@ function QuoteDisplay() {
   return (
     <div>
       
-      <h1>Wisdom of Today: Wedday, January 2nd, 2024 </h1>
+      <h1>Wisdom of Today: Wednday, January 2nd, 2024 </h1>
       <div class="quote-container">
       <blockquote className="quote">{quote}
       </blockquote>
