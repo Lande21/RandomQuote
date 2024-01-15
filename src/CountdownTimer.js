fd91bcf7ff3142775b7259625de92f48388938a4
Rolande <89341905+Lande21@users.noreply.github.com>
2024-01-14 23:33:33 -0600

    daily commits

--- src/CountdownTimer.js
+++ src/CountdownTimer.js
@@ -47,8 +47,8 @@ const CountdownTimer = ({ targetDate }) => {
         <h1 className="countt">Countdown for the NEW YEAR's!!!</h1>
         <h4 className="daily">May GOD strengthen me this year of 2024 as i move my feet and strengthen my faith with GOD. I know that i got this!! I am applying to 3 Grad schools by feb 2st</h4>
         <h4><ul><li>Tim form CMU replied to my email</li>
-        <li>I wen to work 1-5pm and did small projects for deanna</li>
-        <li>I made dinner</li>
+        <li>I went to work 1-5pm and did small projects for deanna</li>
+        <li></li>
         <li>I made dinner</li></ul>
         </h4>
         <p>{timeLeft.days} days</p>
