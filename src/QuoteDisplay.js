6d85d61c547fdd77d262f9c23d32a601b3570c45
Rolande <89341905+Lande21@users.noreply.github.com>
2024-01-16 18:53:11 -0600

    daily commits

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -61,7 +61,7 @@ function QuoteDisplay() {
   return (
     <div>
       
-      <h1>Wisdom of Today: Ty, January 15th, 2024 </h1>
+      <h1>Wisdom of Today: Tuey, January 15th, 2024 </h1>
       <div class="quote-container">
       <blockquote className="quote">{quote}
       </blockquote>
